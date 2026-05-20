fdfd1e74692f6aa083cf3ba754bbf9f234913d12
Kellen Kincaid <kellenceo@gmail.com>
2026-05-20 15:52:51 -0400

    Created signUp method for JoinPage class

--- pages/joinPage.ts
+++ pages/joinPage.ts
@@ -4,6 +4,7 @@ export class JoinPage {
     readonly page: Page
     readonly legalFirstNameField: Locator
     readonly legalLastNameField: Locator
+    readonly emailField: Locator
     readonly phoneNumberField: Locator
     readonly passwordField: Locator
     readonly termsOfUseCheckbox: Locator
@@ -13,9 +14,23 @@ export class JoinPage {
         this.page = page
         this.legalFirstNameField = page.getByRole('textbox', { name: 'Legal First Name' })
         this.legalLastNameField = page.getByRole('textbox', { name: 'Legal Last Name' })
+        this.emailField = page.getByRole('textbox', { name: 'Email' })
         this.phoneNumberField = page.getByRole('textbox', { name: 'Phone Number' })
         this.passwordField = page.getByRole('textbox', { name: 'Password' })
         this.termsOfUseCheckbox = page.getByRole('button', { name: 'I agree to Ezra\'s terms of use' })
         this.submitButton = page.getByRole('button', { name: 'Submit' })
+    }
+
+    async signUp(params: { firstName: string, lastName: string, email: string, phoneNumber: string, password: string }): Promise<void> { // TODO - Return an instance of whatever page this leads to (i think it's the login page)
+        const { firstName, lastName, email, phoneNumber, password, } = params
+        
+        await this.legalFirstNameField.fill(firstName)
+        await this.legalLastNameField.fill(lastName)
+        await this.emailField.fill(email)
+        await this.phoneNumberField.fill(phoneNumber)
+        await this.passwordField.fill(password)
+        
+        await this.termsOfUseCheckbox.click()
+        await this.submitButton.click()
     }
 }
